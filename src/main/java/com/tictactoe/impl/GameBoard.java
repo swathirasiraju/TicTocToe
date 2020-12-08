a68fa1c17c5804df478185393043dc9a2feee18f
swathirasiraju <swathi.raju07@gmail.com>
2020-12-08 15:59:44 +0100

    "intial  Commit"

---
+++ src/main/java/com/tictactoe/impl/GameBoard.java
@@ -0,0 +1,13 @@
+package com.tictactoe.impl;
+
+public class GameBoard {
+	
+	public int validateBoardCountTest(int n) {
+		
+		int boardsize=n*n;
+		
+		return boardsize;
+		
+	}
+	
+}
---
+++ src/test/java/com/tictoctoe/test/ValidationTests.java
@@ -0,0 +1,20 @@
+package com.tictoctoe.test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.tictactoe.impl.GameBoard;
+
+public class ValidationTests {	
+	
+	GameBoard gameboard=new GameBoard();	
+	@Test
+	public void ValidationBoardCounttest() {
+		
+		assertNotEquals("16",gameboard.validateBoardCountTest(3));	
+		
+	}
+	
+	
+}
